56e84ce676d280b18c15707c43eb89fd304d53f6
fynnwilliam <fynnwilliam@gmail.com>
2021-12-10 02:44:38 -0500

    declared an account builder class

---
+++ include/account_builder.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "account.h"
+
+class account_builder
+{
+private:
+    account account_;
+    
+public:
+    account_builder(std::string const&, std::string const&);
+    
+    operator account() const noexcept { return std::move(account_); }
+    
+    account_builder& email(std::string const&)      noexcept;
+    account_builder& last_name(std::string const&)  noexcept;
+    account_builder& first_name(std::string const&) noexcept;
+    account_builder& balance(double)                noexcept;
+    account_builder& id(unsigned)                   noexcept;
+    account_builder& type(account_t)                noexcept;
+};
