64ba2be183baeea3b1642d754cce5458e51049d0
fynnwilliam <fynnwilliam@gmail.com>
2021-12-10 18:45:19 -0500

    updated responds for the default option

--- src/main.cpp
+++ src/main.cpp
@@ -91,11 +91,10 @@ int main()
             gcb.showAccounts();
             break;
         case 7:
-            std::cout << "\nthank you, for banking with us\n";
+            std::cout << "\nthank you, for banking with us" << std::endl;
             break;
         default:
             std::cout << "\nselect any of options 1 through 7\n";
-            exit(0);
         }
     } while (option != 7);
 
