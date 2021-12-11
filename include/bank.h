bc6d10ab186761179f7d966a47333f4f761d565b
fynnwilliam <fynnwilliam@gmail.com>
2021-12-11 09:11:31 -0500

    marked some functions as private

--- include/bank.h
+++ include/bank.h
@@ -9,28 +9,30 @@ private:
     
     unsigned  new_id() const noexcept;
     unsigned _new_id() const noexcept;
-    unsigned      id() const noexcept;
+    unsigned  id()     const noexcept;
     double    amount() const noexcept;
-    bool            valid(std::string const&) const noexcept;
-    std::string const& request(std::string&&) const noexcept;
-    std::string const& last_name() const noexcept;
+
+    std::string const& last_name()  const noexcept;
     std::string const& first_name() const noexcept;
-    std::string const& email() const noexcept;
-    void               account_not_found()    const noexcept;
+    std::string const& email()      const noexcept;
+    void account_not_found()        const noexcept;
+    bool valid(std::string const&)  const noexcept;
+    void check_balance(unsigned)    const noexcept;
+    void deposit(unsigned, double)        noexcept;
+    void withdraw(unsigned, double)       noexcept;
+    void close_account(unsigned)          noexcept;
+    
+    std::string const& request(std::string&&) const noexcept;
+    auto open_account(account_builder&&)            noexcept;
 
 public:
     bank(database& db) : accounts_{db} {}
     
-    auto open_account(account_builder&&)       noexcept;
-    void check_balance(unsigned)         const noexcept;
-    void       deposit(unsigned, double)       noexcept;
-    void      withdraw(unsigned, double)       noexcept;
-    void close_account(unsigned)               noexcept;
-    void list_accounts()                 const noexcept;
-    void close_account() noexcept;
-    void withdraw()      noexcept;
-    void  deposit()      noexcept;
+    void list_accounts() const noexcept;
+    void close_account()       noexcept;
+    void withdraw()            noexcept;
+    void deposit()             noexcept;
     void check_balance() const noexcept;
-    void open_account() noexcept;
-    int inquire() const noexcept;
+    void open_account()        noexcept;
+    int  inquire()       const noexcept;
 };
