92568b80ac679271d0c2465bcede04c9fde7f7b2
fynnwilliam <fynnwilliam@gmail.com>
2021-12-10 23:55:32 -0500

    using bank::inquire() in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,41 +3,32 @@
 int main()
 {
     bank gcb{};
-
+    int option{};
     std::cout << "welcome to console banking! :)\n\n";
 
     do
     {
-        std::cout << "\n"
-                 << "\t1. open an account\n"
-                 << "\t2. balance enquiry\n"
-                 << "\t3. deposit\n"
-                 << "\t4. withdrawal\n"
-                 << "\t5. close an account\n"
-                 << "\t6. show accounts\n"
-                 << "\t7. exit\n"
-                 << "\nselect an option: ";
-
+        gcb.inquire();
         std::cin >> option;
 
         switch (option)
         {
-        case 1:
-            gcb.open_account();                               break;
-        case 2:
-            gcb.check_balance();                              break;
-        case 3:
-            gcb.deposit();                                    break;
-        case 4:
-            gcb.withdraw();                                   break;
-        case 5:
-            gcb.close_account();                              break;
-        case 6:
-            gcb.list_accounts();                              break;
-        case 7:
-            std::cout << "\nthave a great day!" << std::endl; break;
-        default:
-            std::cout << "\nselect an option\n";
+            case 1:
+                gcb.open_account();                               break;
+            case 2:
+                gcb.check_balance();                              break;
+            case 3:
+                gcb.deposit();                                    break;
+            case 4:
+                gcb.withdraw();                                   break;
+            case 5:
+                gcb.close_account();                              break;
+            case 6:
+                gcb.list_accounts();                              break;
+            case 7:
+                std::cout << "\nthave a great day!" << std::endl; break;
+            default:
+                std::cout << "\nselect among options 1 through 7\n";
         }
     } while (option != 7);
 
