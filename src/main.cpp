4f7c199599ed75df1aa9a141416251e72aee42f0
fynnwilliam <fynnwilliam@gmail.com>
2021-12-11 21:09:38 -0500

    updated while loop

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,9 @@
 int main()
 {
     bank gcb{database::instance()};
+    
     while (int option = gcb.inquire())
-    {
-        switch (option)
-        {
-            case 1:
-                gcb.open_account();  break;
-            case 2:
-                gcb.check_balance(); break;
-            case 3:
-                gcb.deposit();       break;
-            case 4:
-                gcb.withdraw();      break;
-            case 5:
-                gcb.close_account(); break;
-            case 6:
-                gcb.list_accounts(); break;
-            default:
-                continue;
-        }
-    }
+        gcb.deliver(option);
     
     std::cout << "\n\tnice, have a great day!" << std::endl;
 
