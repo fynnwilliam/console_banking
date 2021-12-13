15c97cb4e99b8bc0247bdd0249626928e0bdfdcc
fynnwilliam <fynnwilliam@gmail.com>
2021-12-12 19:47:29 -0500

    changed type for activity::from_ and activity::to_

--- include/activity.h
+++ include/activity.h
@@ -12,14 +12,14 @@ private:
     activity_t          type_;
     clock_t::time_point time_{clock_t::now()};
     double              amount_;
-    std::string         from_;
-    std::string         to_;
+    unsigned            from_;
+    unsigned            to_;
     
 public:
     activity(activity_t type, double amount)
         : type_{type}, amount_{amount} {}
     
-    activity(activity_t type, double amount, std::string const& f, std::string const& t)
+    activity(activity_t type, double amount, unsigned f, unsigned t)
         : type_{type}, amount_{amount}, from_{f}, to_{t} {}
     
     activity_t          name()   const noexcept;
