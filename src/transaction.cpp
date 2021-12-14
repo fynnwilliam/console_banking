958b2c9b52c6bddf617ff5c6c3e694458104a718
fynnwilliam <fynnwilliam@gmail.com>
2021-12-13 20:34:16 -0500

    updated d'tor

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -22,13 +22,13 @@ bool transaction::empty() const noexcept
     return logs_.empty();
 }
 
-auto transaction::find(std::uint64_t id) const noexcept
+std::optional<activity> transaction::find(std::uint64_t id) const noexcept
 {
     logs_.contains(id) ? std::make_optional(logs_.at(id)) : std::nullopt;
 }
 
 transaction::~transaction()
 {
-    if (!logs_.empty()) write();
+    if (!empty()) write();
 }
 
