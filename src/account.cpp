0093f31ff515edc73f19b95303100c461ac87fe0
fynnwilliam <fynnwilliam@gmail.com>
2021-12-15 18:47:18 -0500

    removed account::read_logs() from std::ifstream overload

--- src/account.cpp
+++ src/account.cpp
@@ -133,7 +133,6 @@ std::ifstream& operator>>(std::ifstream& ifs, account& acc)
         >> acc.first_name_
         >> acc.email_
         >> acc.balance_;
-           acc.read_logs();
 
     return ifs;
 }
