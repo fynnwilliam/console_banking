5456b613b6b6cf64145d5a161293e1d030f2a75d
fynnwilliam <fynnwilliam@gmail.com>
2021-12-15 21:13:42 -0500

    parsing a parameter to c'tor for generate_id class

--- include/id_generator.h
+++ include/id_generator.h
@@ -8,10 +8,14 @@ private:
     static std::uint64_t read_id()    noexcept;
     void save_last_id()         const noexcept;
     
-    id_generator() = default;
+    id_generator(std::uint64_t i) : id_{i} {}
     
 public:
-    static id_generator& instance()   noexcept;
+    static id_generator& instance()   noexcept
+    {
+        static id_generator id{};
+        return id;
+    }
     
     ~id_generator();
 };
