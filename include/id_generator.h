bde3644cc6dffcb4576150bfae2ad8ad4c178edd
fynnwilliam <fynnwilliam@gmail.com>
2021-12-15 21:02:32 -0500

    created a generate_id class

---
+++ include/id_generator.h
@@ -0,0 +1,18 @@
+#include <atomic>
+
+class id_generator
+{
+private:
+    std::atomic<std::uint64_t> id_{};
+    
+    static std::uint64_t read_id()    noexcept;
+
+    void save_last_id()         const noexcept;
+    
+    id_generator() = default;
+    
+public:
+    static id_generator& instance()   noexcept;
+    
+    ~id_generator();
+};
