9710f65d2c90e5a9f1bfc33318e58b69dcad9677
fynnwilliam <fynnwilliam@gmail.com>
2021-12-15 21:07:27 -0500

    implemented id_generator::instance()

--- include/id_generator.h
+++ include/id_generator.h
@@ -6,7 +6,6 @@ private:
     std::atomic<std::uint64_t> id_{};
     
     static std::uint64_t read_id()    noexcept;
-
     void save_last_id()         const noexcept;
     
     id_generator() = default;
