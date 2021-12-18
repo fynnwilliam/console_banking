d1f17388894781db19e52a75e82905db29ad2329
fynnwilliam <fynnwilliam@gmail.com>
2021-12-18 16:20:45 -0500

    deleting copy assignment operator for id_generator

--- include/id_generator.h
+++ include/id_generator.h
@@ -12,6 +12,7 @@ private:
     
     id_generator(std::uint64_t i) : id_{i} {}
     id_generator(id_generator const&) = delete;
+    auto operator=(id_generator const&) = delete;
     
 public:
     static id_generator& instance()      noexcept
