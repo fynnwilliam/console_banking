4231310529ed3a6cd2e32f2a2f4ce5a3a98cf185
fynnwilliam <fynnwilliam@gmail.com>
2022-01-04 16:50:42 -0500

    revert changes to bank::request

--- src/bank.cpp
+++ src/bank.cpp
@@ -175,7 +175,7 @@ std::string& bank::request(std::string&& statement) const noexcept
     std::cout << statement << ": ";
     std::getline(std::cin, input);
     
-    return trim(input).size() ? input : request(statement));
+    return trim(input).size() ? input : request(std::move(statement));
 }
 
 std::string& bank::trim(std::string& s) const noexcept
