8e92cde97021202a34b9319929be193bee428f7d
fynnwilliam <fynnwilliam@gmail.com>
2022-03-27 13:08:17 -0400

    Apply clang-format

--- include/account_builder.h
+++ include/account_builder.h
@@ -1,23 +1,19 @@
 #pragma once
 #include "account.h"
 
-class account_builder
-{
+class account_builder {
 private:
-    account account_;
-    
+  account account_;
+
 public:
-    account_builder(std::string, std::string);
-    
-    operator account()                        const  noexcept
-    {
-        return std::move(account_); 
-    }
-    
-    account_builder& email(std::string const&)      noexcept;
-    account_builder& last_name(std::string const&)  noexcept;
-    account_builder& first_name(std::string const&) noexcept;
-    account_builder& balance(double)                noexcept;
-    account_builder& id(unsigned)                   noexcept;
-    account_builder& type(account_t)                noexcept;
+  account_builder(std::string, std::string);
+
+  operator account() const noexcept { return std::move(account_); }
+
+  account_builder &email(std::string const &) noexcept;
+  account_builder &last_name(std::string const &) noexcept;
+  account_builder &first_name(std::string const &) noexcept;
+  account_builder &balance(double) noexcept;
+  account_builder &id(unsigned) noexcept;
+  account_builder &type(account_t) noexcept;
 };
