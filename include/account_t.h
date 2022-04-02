bae2d8b35f3b4a07807696e62dfb9f5f550c7bb9
fynnwilliam <fynnwilliam@gmail.com>
2022-04-02 12:55:54 -0400

    apply clang-format

--- include/account_t.h
+++ include/account_t.h
@@ -1,10 +1,3 @@
 #pragma once
 
-enum class account_t
-{
-    savings,
-    checking,
-    current,
-    foreign,
-    investment
-};
+enum class account_t { savings, checking, current, foreign, investment };
