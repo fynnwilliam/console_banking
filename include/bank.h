e621e71520e2b565f4922a2175aac31077f7d4d4
fynnwilliam <fynnwilliam@gmail.com>
2022-04-02 13:21:02 -0400

    apply clang-format

--- include/bank.h
+++ include/bank.h
@@ -2,51 +2,50 @@
 
 #include "database.h"
 
-class bank
-{
+class bank {
 private:
-    database& accounts_;
-    
-    unsigned  new_id()                  const noexcept;
-    unsigned _new_id()                  const noexcept;
-    unsigned  id()                      const noexcept;
-    double    amount()                  const noexcept;
-    int       option()                  const noexcept;
-    
-    void list_accounts()                const noexcept;
-    void list_transactions()            const noexcept;
-    void check_balance()                const noexcept;
-    void close_account()                      noexcept;
-    void withdraw()                           noexcept;
-    void deposit()                            noexcept;
-    void open_account()                       noexcept;
-    int  _inquire()                     const noexcept;
-    void deliver(int)                         noexcept;
-    void deprecate_account(unsigned)          noexcept;
-    void archive_account(unsigned)      const noexcept;
+  database &accounts_;
 
-    std::string last_name()             const noexcept;
-    std::string first_name()            const noexcept;
-    std::string email()                 const noexcept;
-    
-    void account_not_found()            const noexcept;
-    void low_funds()                    const noexcept;
-    bool valid(std::string const&)      const noexcept;
-    bool inspect(std::string const&)    const noexcept;
-    void check_balance(unsigned)        const noexcept;
-    void deposit(unsigned, double)            noexcept;
-    void withdraw(unsigned, double)           noexcept;
-    void close_account(unsigned)              noexcept;
-    void list_transactions(unsigned)    const noexcept;
-    void trim_front(std::string&)       const noexcept;
-    void trim_back(std::string&)        const noexcept;
-    
-    std::string& request(std::string&&) const noexcept;
-    std::string& trim(std::string&)     const noexcept;
-    auto open_account(account_builder&&)      noexcept;
+  unsigned new_id() const noexcept;
+  unsigned _new_id() const noexcept;
+  unsigned id() const noexcept;
+  double amount() const noexcept;
+  int option() const noexcept;
+
+  void list_accounts() const noexcept;
+  void list_transactions() const noexcept;
+  void check_balance() const noexcept;
+  void close_account() noexcept;
+  void withdraw() noexcept;
+  void deposit() noexcept;
+  void open_account() noexcept;
+  int _inquire() const noexcept;
+  void deliver(int) noexcept;
+  void deprecate_account(unsigned) noexcept;
+  void archive_account(unsigned) const noexcept;
+
+  std::string last_name() const noexcept;
+  std::string first_name() const noexcept;
+  std::string email() const noexcept;
+
+  void account_not_found() const noexcept;
+  void low_funds() const noexcept;
+  bool valid(std::string const &) const noexcept;
+  bool inspect(std::string const &) const noexcept;
+  void check_balance(unsigned) const noexcept;
+  void deposit(unsigned, double) noexcept;
+  void withdraw(unsigned, double) noexcept;
+  void close_account(unsigned) noexcept;
+  void list_transactions(unsigned) const noexcept;
+  void trim_front(std::string &) const noexcept;
+  void trim_back(std::string &) const noexcept;
+
+  std::string &request(std::string &&) const noexcept;
+  std::string &trim(std::string &) const noexcept;
+  auto open_account(account_builder &&) noexcept;
 
 public:
-    bank(database& db) : accounts_{db} {}
+  bank(database &db) : accounts_{db} {}
 
-    void inquire()                            noexcept;
+  void inquire() noexcept;
 };
