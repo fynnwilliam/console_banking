dab9f6d1746382a3e17024937010051826e56e6a
fynnwilliam <fynnwilliam@gmail.com>
2022-04-02 15:59:53 -0400

    apply clang-format

--- include/transaction.h
+++ include/transaction.h
@@ -1,31 +1,30 @@
 #pragma once
-#include <unordered_map>
-#include <string>
 #include <optional>
+#include <string>
+#include <unordered_map>
 
 #include "activity.h"
 #include "id_generator.h"
 
-class transaction
-{
+class transaction {
 private:
-    std::unordered_map<std::uint64_t, activity> logs_;
-    id_generator& id_{id_generator::instance()};
-    
-    typedef decltype(logs_.cbegin()) t_citr;
-    typedef decltype(logs_.begin())   t_itr;
+  std::unordered_map<std::uint64_t, activity> logs_;
+  id_generator &id_{id_generator::instance()};
+
+  typedef decltype(logs_.cbegin()) t_citr;
+  typedef decltype(logs_.begin()) t_itr;
 
 public:
-    transaction() = default;
-    
-    void   log(activity&&)                              noexcept;
-    void   read(std::string const&)                     noexcept;
-    void   write(std::string const&)              const noexcept;
-    bool   empty()                                const noexcept;
-    t_itr  begin()                                      noexcept;
-    t_citr begin()                                const noexcept;
-    t_itr  end()                                        noexcept;
-    t_citr end()                                  const noexcept;
-    
-    std::optional<activity> find(std::uint64_t)   const noexcept;
+  transaction() = default;
+
+  void log(activity &&) noexcept;
+  void read(std::string const &) noexcept;
+  void write(std::string const &) const noexcept;
+  bool empty() const noexcept;
+  t_itr begin() noexcept;
+  t_citr begin() const noexcept;
+  t_itr end() noexcept;
+  t_citr end() const noexcept;
+
+  std::optional<activity> find(std::uint64_t) const noexcept;
 };
--- src/transaction.cpp
+++ src/transaction.cpp
@@ -2,57 +2,38 @@
 
 #include "transaction.h"
 
-void transaction::log(activity&& a) noexcept
-{
-    logs_.insert({++id_, a});
+void transaction::log(activity &&a) noexcept { logs_.insert({++id_, a}); }
+
+void transaction::read(std::string const &acc_id) noexcept {
+  std::ifstream file{acc_id, std::ios::in};
+  std::uint64_t t_id{};
+  activity act{{}, {}};
+
+  while (file >> t_id >> act)
+    logs_.insert({t_id, act});
 }
 
-void transaction::read(std::string const& acc_id) noexcept
-{
-    std::ifstream file{acc_id, std::ios::in};
-    std::uint64_t t_id{}; activity act{{}, {}};
-    
-    while (file >> t_id >> act)
-        logs_.insert({t_id, act});
+void transaction::write(std::string const &acc_id) const noexcept {
+  std::ofstream file{acc_id, std::ios::trunc};
+  for (auto const &[t_id, act] : logs_) {
+    file << t_id << '\n';
+    file << act;
+  }
 }
 
-void transaction::write(std::string const& acc_id) const noexcept
-{
-    std::ofstream file{acc_id, std::ios::trunc};
-    for (auto const& [t_id, act] : logs_)
-    {   
-        file << t_id << '\n';
-        file << act;
-    }
+bool transaction::empty() const noexcept { return logs_.empty(); }
+
+std::optional<activity> transaction::find(std::uint64_t id) const noexcept {
+  auto const &act{logs_.find(id)};
+  return act != logs_.end() ? std::make_optional(act->second) : std::nullopt;
 }
 
-bool transaction::empty() const noexcept
-{
-    return logs_.empty();
+transaction::t_itr transaction::begin() noexcept { return logs_.begin(); }
+
+transaction::t_citr transaction::begin() const noexcept {
+  return logs_.cbegin();
 }
 
-std::optional<activity> transaction::find(std::uint64_t id) const noexcept
-{
-    auto const& act{logs_.find(id)};
-    return act != logs_.end() ? std::make_optional(act->second) : std::nullopt;
-}
+transaction::t_itr transaction::end() noexcept { return logs_.end(); }
 
-transaction::t_itr transaction::begin() noexcept
-{
-    return logs_.begin();
-}
-
-transaction::t_citr transaction::begin() const noexcept
-{
-    return logs_.cbegin();
-}
-
-transaction::t_itr transaction::end() noexcept
-{
-    return logs_.end();
-}
-
-transaction::t_citr transaction::end() const noexcept
-{
-    return logs_.cend();
-}
+transaction::t_citr transaction::end() const noexcept { return logs_.cend(); }
