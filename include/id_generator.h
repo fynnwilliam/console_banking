f517cbd0399b1cb10ce9cad14aeed5f9426ceb00
fynnwilliam <fynnwilliam@gmail.com>
2022-04-02 15:51:03 -0400

    apply clang-format

--- include/id_generator.h
+++ include/id_generator.h
@@ -2,27 +2,25 @@
 #include <atomic>
 #include <fstream>
 
-class id_generator
-{
+class id_generator {
 private:
-    std::atomic<std::uint64_t> id_{};
-    
-    static std::uint64_t read_id()       noexcept;
-    void save_last_id()            const noexcept;
-    
-    id_generator(std::uint64_t i) : id_{i} {}
-    id_generator(id_generator const&) = delete;
-    id_generator& operator=(id_generator const&) = delete;
-    
+  std::atomic<std::uint64_t> id_{};
+
+  static std::uint64_t read_id() noexcept;
+  void save_last_id() const noexcept;
+
+  id_generator(std::uint64_t i) : id_{i} {}
+  id_generator(id_generator const &) = delete;
+  id_generator &operator=(id_generator const &) = delete;
+
 public:
-    static id_generator& instance()      noexcept
-    {
-        static id_generator id{read_id()};
-        return id;
-    }
-    
-    std::uint64_t operator++()           noexcept;
-    std::uint64_t operator++(int)        noexcept;
-    
-    ~id_generator();
+  static id_generator &instance() noexcept {
+    static id_generator id{read_id()};
+    return id;
+  }
+
+  std::uint64_t operator++() noexcept;
+  std::uint64_t operator++(int) noexcept;
+
+  ~id_generator();
 };
--- src/id_generator.cpp
+++ src/id_generator.cpp
@@ -1,30 +1,19 @@
 #include "id_generator.h"
 
-std::uint64_t id_generator::read_id() noexcept
-{
-    std::uint64_t id{};
-    if (std::ifstream source{"id", std::ios::in})
-        source >> id;
-    return id;
+std::uint64_t id_generator::read_id() noexcept {
+  std::uint64_t id{};
+  if (std::ifstream source{"id", std::ios::in})
+    source >> id;
+  return id;
 }
 
-void id_generator::save_last_id() const noexcept
-{
-    if (std::ofstream source{"id", std::ios::trunc})
-        source << id_.load();
+void id_generator::save_last_id() const noexcept {
+  if (std::ofstream source{"id", std::ios::trunc})
+    source << id_.load();
 }
 
-std::uint64_t id_generator::operator++() noexcept
-{
-    return ++id_;
-}
+std::uint64_t id_generator::operator++() noexcept { return ++id_; }
 
-std::uint64_t id_generator::operator++(int) noexcept
-{
-    return id_++;
-}
+std::uint64_t id_generator::operator++(int) noexcept { return id_++; }
 
-id_generator::~id_generator()
-{
-    save_last_id();
-}
+id_generator::~id_generator() { save_last_id(); }
